35e120bf422ef8218a7a1f798d59de2bb0c905da
Utsav Rai <utsavrai890@gmail.com>
2023-11-06 22:43:05 +0530

    logout screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rentapp/localstorage/local_storage.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () async {
+                  context.pushNamed('LogIn');
+                  await LocalStorageService().deleteDataFromStorage('db_token');
+                },
+                child: const Text('LogOut'),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
